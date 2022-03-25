3d76eb321016306c59c62fbd20c7d9aa12b9c72e
Dane <danejwhitfield@gmail.com>
2022-03-25 19:06:01 +0000

    updated

---
+++ app.js
@@ -0,0 +1,28 @@
+const fs = require("fs/promises");
+const homedir = require("os").homedir();
+const home = `${homedir}/Library/Application Support/Code/User/settings.json`;
+
+function shrelify() {
+  const shrelSettings = `{
+    "[javascript]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode",
+    },
+    "[html]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode",
+    },
+    "editor.formatOnSave": true,
+    "bracketPairColorizer.depreciation-notice": false,
+    "editor.bracketPairColorization.enabled": true,
+    "workbench.colorTheme": "United Ubuntu",
+    "workbench.preferredDarkColorTheme": "United Ubuntu",
+    "window.zoomLevel": -1,
+  
+    "workbench.colorCustomizations": {
+      "terminal.foreground": "#00fd61",
+    },
+  }`;
+
+  fs.writeFile(home, shrelSettings);
+}
+
+module.exports = shrelify;
